08437c58737a50b16b3c486c17de602840132689
Luke Hudson <lukeletters@gmail.com>
2015-06-19 17:44:35 +0200

    BUGFIX: Allow use on frontend
    'Chosen' plugin assumed, but on front-end this is not
    a valid assumption.

--- js/TagField.js
+++ js/TagField.js
@@ -9,22 +9,24 @@
 	 * we'll see an extra field where there should be only one.
 	 */
 	$.fn.chosenDestroy = function () {
-		$(this)
-			.show() // The field needs to be visible so Select2 evaluates the width correctly.
-			.removeClass('chzn-done')
-			.removeClass('has-chzn')
-			.next()
-			.remove();
-
-		return $(this);
+		var $this = $(this);
+		if ($this.siblings('.chzn-container').length) {
+			$this
+				.show() // The field needs to be visible so Select2 evaluates the width correctly.
+				.removeClass('chzn-done')
+				.removeClass('has-chzn')
+				.next()
+				.remove();
+		}
+		return $this;
 	};
 
 	$.entwine('ss', function ($) {
 
-		$('.ss-tag-field + .chzn-container').entwine({
+		$('.ss-tag-field').entwine({
 			applySelect2: function () {
 				var self = this,
-					$select = $(this).prev();
+					$select = $(this);
 
 				// There is a race condition where Select2 might not
 				// be bound to jQuery yet. So here we make sure Select2
